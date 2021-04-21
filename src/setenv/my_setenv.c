3a624ae0475372168ee103307eb4868f2e749d46
Ewen LE QUERE <ewen@MacBook-Air-de-Ewen.local>
2021-04-22 00:11:23 +0200

    [FINISH] command setenv at 100%

--- src/setenv/my_setenv.c
+++ src/setenv/my_setenv.c
@@ -8,6 +8,22 @@
 #define _GNU_SOURCE
 #include "../../include/my_src.h"
 
+static char **anti_backslach2(char **str, int i, int j)
+{
+    if (str[i][j] == '\n')
+        str[i][j] = '\0';
+    return str;
+}
+
+
+static char **anti_backslach(char **str)
+{
+    for (int i = 0; str[i]; i++)
+        for (int j = 0; str[i][j]; j++)
+            str = anti_backslach2(str, i, j);
+    return str;
+}
+
 void my_setenv(env_t *envi, char **str)
 {
     int size = my_tablen(envi->tab);
@@ -16,5 +32,6 @@ void my_setenv(env_t *envi, char **str)
     new_tab[size] = my_strdup(str[1]);
     envi->tab = new_tab;
     envi->tab[size + 1] = NULL;
+    envi->tab = anti_backslach(envi->tab);
     my_printf("%t", envi->tab);
 }
