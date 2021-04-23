3237e666366726fa56f37821c0f6eeb3eb52ab81
Ewen LE QUERE <ewen@MacBook-Air-de-Ewen.local>
2021-04-24 01:55:59 +0200

    [ADD] first version: unsetenv command, upgrade exit and setenv command

--- src/my_shell/diff_bin_cmd.c
+++ src/my_shell/diff_bin_cmd.c
@@ -13,19 +13,20 @@ void diff_bin_cmd(env_t *envi, char *buffer)
     char **str1 = split_getlin(buffer);
     int enviro = my_strcmp(str1[0], "env\n");
     int exi = my_strcmp(str1[0], "exit\n");
+    int exi2 = my_strcmp(str1[0], "exit");
     int setenv = my_strcmp(str1[0], "setenv");
     int setenv2 = my_strcmp(str1[0], "setenv\n");
     int unsetenv = my_strcmp(str1[0], "unsetenv");
+    int unsetenv2 = my_strcmp(str1[0], "unsetenv\n");
 
     if (enviro == 0)
         my_env(envi);
     else if (setenv == 0 || setenv2 == 0)
         my_setenv(envi, str1);
-    else if (unsetenv == 0)
+    else if (unsetenv == 0 || unsetenv2 == 0)
         my_unsetenv(envi, str1);
-    else if (exi == 0)
-        exit(0);
-    else {
+    else if (exi == 0 || exi2 == 0)
+        my_exit(str1);
+    else
         check_command(envi->tab, str1);
-    }
 }
