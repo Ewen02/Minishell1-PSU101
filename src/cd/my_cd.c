f5b839da2ac0c80ef9aaca5a20549a51d597b7a1
Ewen <ewen.le-quere@epitech.eu>
2021-04-30 16:00:15 +0200

    [ADD] more fonctionnality for cd command, 'cd  '

--- src/cd/my_cd.c
+++ src/cd/my_cd.c
@@ -15,18 +15,39 @@ static char **change_env(env_t *envi, char *str)
     return envi->tab;
 }
 
+static char *go_home(env_t *envi)
+{
+    char **home = NULL;
+
+    for (int i = 0; envi->tab[i]; i++)
+        if (my_strncmp(envi->tab[i], "HOME", 4) == 0)
+            home = my_str_to_tab(envi->tab[i], '=');
+    return home[1];
+}
+
 void my_cd(env_t *envi, char **str)
 {
-    int size = my_strlen(str[1]);
-    char *pwd = malloc(sizeof(char) * (size + 2));
+    int sizetab = my_strlentab(str);
+    char *pwd = malloc(sizeof(char) * 256);
     char *path = malloc(sizeof(char) * 256);
+    char *home = malloc(sizeof(char) * 256);
 
-    pwd = anti_backn_string(str[1]);
-    chdir(pwd);
     getcwd(path, 256);
     path = my_strcat("PWD=", path);
+
+    if (sizetab == 2) {
+        pwd = anti_backn_string(str[1]);
+        chdir(pwd);
+    }
     if (my_strcmp(pwd, "..") == 0 || my_strcmp(pwd, "../") == 0)
         envi->tab = change_env(envi, path);
+    if (sizetab == 1) {
+        home = go_home(envi);
+        chdir(home);
+        envi->tab = change_env(envi, path);
+    }
+    if (my_strcmp(pwd, "-") == 0)
+        my_printf("toto\n");
     else
         envi->tab = change_env(envi, path);
 }
